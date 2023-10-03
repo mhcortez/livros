33ec93be1fce6d1e95cedf9a7b052f1c54e6c909
Marcelo Cortez <mhcortez@gmail.com>
2023-10-03 15:48:01 -0300

    novo
    novo

--- project/urls.py
+++ project/urls.py
@@ -10,7 +10,3 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),]
